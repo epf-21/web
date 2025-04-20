cc6d2e941c5b84993556f64f92466dfa847be54e
efrain <efrainperalta.f@gmail.com>
2025-04-20 09:32:55 -0400

    chore: configuracion de standard

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,19 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
+import express, { json } from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(cors())
 app.use(json())
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send('Nuevo proyecto')
 })
 
-
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
