d8d12f2b5ad6f1807111430620e824a0304e4bc3
adrisad <22082002andrademedina@gmail.com>
2025-04-20 09:14:49 -0400

    feat: estructura del componente puerta

---
+++ frontend/components/custom/Puerta.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Puerta() {
+  return (
+    <div class="door-container">
+        <div class="door-frame"></div>
+        <div class="door">
+            <div class="doorknob"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Puerta
