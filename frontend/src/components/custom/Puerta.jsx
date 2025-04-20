3287d88456ee2f15278b256deb3d9c027393c2ab
efrain <efrainperalta.f@gmail.com>
2025-04-20 10:31:31 -0400

    refactor(frontend): reestructuracion de carpeta

---
+++ frontend/src/components/custom/Puerta.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Puerta() {
+  return (
+    <div className="door-container">
+      <div className="door-frame"></div>
+      <div className="door">
+        <div className="doorknob"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Puerta
