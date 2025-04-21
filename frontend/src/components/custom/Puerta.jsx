ac2b992e05226afb8bf3d948e2b5fc690454675b
adrisad <22082002andrademedina@gmail.com>
2025-04-21 17:05:35 -0400

    style: Quitar fondo blanco y color de texto a blanco

--- frontend/src/components/custom/Puerta.jsx
+++ frontend/src/components/custom/Puerta.jsx
@@ -6,12 +6,12 @@ export default function Puerta({msg,emoji}) {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center">
       <div className="relative w-48 h-64 cursor-pointer overflow-hidden" onClick={toggleDoor}>
         
         <div className="absolute w-48 h-64 bg-amber-800 border-4 border-amber-900 flex justify-center items-center text-9xl">{emoji}</div>
         <div 
-          className={`absolute w-44 h-60 bg-amber-700 border-2 border-amber-900 top-2 left-2 transition-transform duration-1000 ease-in-out ${
+          className={`absolute w-44 h-60 bg-amber-700 border-2 border-amber-900 top-2 left-2 transition-transform duration-2000 ease-in-out ${
             isOpen ? 'translate-x-44' : 'translate-x-0'
           }`}
         >
@@ -19,7 +19,7 @@ export default function Puerta({msg,emoji}) {
         </div>
       </div>
       
-      <p className="mt-4 text-center text-gray-700 font-sans">
+      <p className="mt-4 text-center text-amber-50 font-sans">
           {msg}
       </p>
     </div>
