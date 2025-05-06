25884db32ed55b9120faa89d30748996876c4f42
Ronald Krings Revollo <202202861@est.umss.edu>
2025-05-05 22:49:16 -0400

    Revert "feat: pregunta interactiva pipePuzzle"
    This reverts commit 1e2452a3b415c4c70eb0275b0ed226114d053259.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import GameDoor from './pages/GameDoor.jsx'
-import PipePuzzle from './pages/PipePuzzle.jsx'
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/GameDoor' element={<GameDoor />} />
-        <Route path='/PipePuzzle' element={<PipePuzzle />} />
       </Routes>
     </BrowserRouter>
   )
