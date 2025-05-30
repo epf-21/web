b7e1d9b607851f3e15113325e8a09ad662ddc47b
efrain <efrainperalta.f@gmail.com>
2025-05-30 08:58:49 -0400

    feat: agregar login y register básico

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ import express, { json } from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import { errorHandler } from './middlewares/errorHandler.js'
-
+import authRouter from './modules/auth/auth.routes.js'
 const app = express()
 
 app.use(cors())
@@ -12,6 +12,8 @@ app.use(morgan('dev'))
 app.get('/', (req, res) => {
   res.send('Editor de preguntas interactivas')
 })
+
+app.use('/api/auth', authRouter)
 
 app.use(errorHandler)
 
---
+++ backend/src/modules/auth/auth.service.js
@@ -0,0 +1,49 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { AuthModel } from './auth.model.js'
+import { JWT_SECRET } from '../../config/env.js'
+
+export class AuthService {
+  static async register(data) {
+    const { name, email, password, rol, phone } = data
+    const exist = await AuthModel.findUserByEmail(email)
+    if (exist) {
+      throw new Error('El correo ya está registrado')
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await AuthModel.create({
+      nombre: name,
+      email,
+      contrasenia: hashedPassword,
+      rol,
+      telefono: phone
+    })
+    return { id: user.id, email: user.email }
+  }
+
+  static async login(data) {
+    const { email, password } = data
+    const user = await AuthModel.findUserByEmail(email)
+    if (!user) {
+      throw new Error('Usuario no encontrado')
+    }
+
+    const valid = await bcrypt.compare(password, user.contrasenia)
+    if (!valid) {
+      throw new Error('Contraseña incorrecta')
+    }
+    const token = jwt.sign({ id: user.id, email: user.email, rol: user.rol }, JWT_SECRET, { expiresIn: '1d' })
+
+    return {
+      user: {
+        id: user.id,
+        name: user.nombre,
+        rol: user.rol,
+        email: user.email
+      },
+      token
+    }
+  }
+}
