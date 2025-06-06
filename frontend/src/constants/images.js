39aed781afd465531c66664051fedac2ae0656f0
efrain <efrainperalta.f@gmail.com>
2025-06-06 10:04:47 -0400

    refactor: extraer la constante a otro archivo

---
+++ frontend/src/constants/images.js
@@ -0,0 +1,102 @@
+export const draggableItemsData = [
+  {
+    id: 1,
+    name: 'Plato',
+    imageUrl: 'plate.png',
+    x: 0,
+    y: 0,
+    width: 220,
+    height: 220,
+    group: 1
+  },
+  {
+    id: 2,
+    name: 'Galleta',
+    imageUrl: 'saltine_cracker.png',
+    x: 0,
+    y: 0,
+    width: 128,
+    height: 128,
+    group: 2
+  },
+  {
+    id: 3,
+    name: 'Cuchara',
+    imageUrl: 'soup_spoon.png',
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 100,
+    group: 3
+  },
+  {
+    id: 4,
+    name: 'Taza de Café',
+    imageUrl: 'mug.png',
+    x: 0,
+    y: 0,
+    width: 144,
+    height: 144,
+    group: 4
+  },
+  {
+    id: 5,
+    name: 'Cuernito',
+    imageUrl: 'croissant.png',
+    x: 0,
+    y: 0,
+    width: 128,
+    height: 128,
+    group: 5
+  },
+  {
+    id: 6,
+    name: 'Pan blanco',
+    imageUrl: 'bread_white.png',
+    x: 0,
+    y: 0,
+    width: 128,
+    height: 128,
+    group: 0
+  },
+  {
+    id: 7,
+    name: 'Pastel',
+    imageUrl: 'cake.png',
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 100,
+    group: 0
+  },
+  {
+    id: 8,
+    name: 'Leche',
+    imageUrl: 'milk.png',
+    x: 0,
+    y: 0,
+    width: 128,
+    height: 128,
+    group: 0
+  },
+  {
+    id: 9,
+    name: 'Hamburguesa',
+    imageUrl: 'burger.png',
+    x: 0,
+    y: 0,
+    width: 128,
+    height: 128,
+    group: 0
+  },
+  {
+    id: 10,
+    name: 'Caja Cereal',
+    imageUrl: 'cereal_box.png',
+    x: 0,
+    y: 0,
+    width: 128,
+    height: 128,
+    group: 0
+  }
+]
